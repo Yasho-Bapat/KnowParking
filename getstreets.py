a361b0ba713d88d52043184c0b6b628227c01039
yashobapat <f20210006@goa.bits-pilani.ac.in>
2025-06-11 11:33:18 +0530

    Initial commit

---
+++ getstreets.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+
+API_KEY = 'HM3NdfEtHpDdg9mDIAXp'
+
+def get_public_IP():
+    response = requests.get('https://api.ipify.org')
+    return response.text
+
+def get_geolocation():
+    response = requests.get(f"https://api.maptiler.com/geolocation/ip.json?key={API_KEY}").json()
+    lat, long = response['latitude'], response['longitude']
+    return lat, long
+
+def get_map(lat, long, mapId):
+    response = requests.get(f"https://api.maptiler.com/maps/{mapId}/static/{long},{lat},1/512x512.webp")
+    return response.text
+
+
+def get_parking_overlays(dataID):
+    response = requests.get(f"https://api.maptiler.com/data/{dataID}/features.json?key={API_KEY}")
+    parking_info_map = response.json()
+    with open("parking_pune.json", "w") as f:
+        json.dump(parking_info_map, f, indent=4)
+
+    return parking_info_map
+
+dataid = "01975d8a-630c-7dc7-b6ff-7175c84fa2ec"
+print(get_parking_overlays(dataid))
